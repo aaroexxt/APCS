472256b75c08b31af8da411d18ec3a9f9bb26ff2
Aaron Becker <aaron.becker@gmail.com>
2019-08-22 08:18:54 -0700

    OutputLab

---
+++ OutputLab/MyFirstProgram.java
@@ -0,0 +1,54 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        /*****PART 1
+        System.out.println("Hello, world!");
+        System.out.println();
+        System.out.println("This is my very first program.");
+        System.out.println("Hooray!");
+         */
+        
+        /*****PART 2
+        System.out.println("Hello, world!");
+        System.out.println("I am learning to program in Java.\nI hope it is a lot of fun!\n\nI hope I get a good grade!\n\nMaybe I'll change my major to Computer Science.");
+         */
+        
+        /*****PART 3
+        System.out.println("A \"quoted\" String is\n\'much\' better if you learn\nthe rules of \"escape sequences.\"\n\nAlso, \"\" represents an empty String.\nDon\'t forget: use \\\" instead of \"!\n\'\' is not the same as \"");
+         */
+        
+        /*****P1.1
+         * System.out.println("+----+\n|Dave|\n+----+");
+         */
+        
+        /*****P1.2
+        System.out.println(" /$$$$$$\\\n/$$__  $$    \\\n| $$  \\ $$  /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$\n| $$$$$$$$ |____  $$ /$$__  $$ /$$__  $$| $$__  $$\n| $$__  $$  /$$$$$$$| $$  \\__/| $$  \\ $$| $$  \\ $$\n| $$  | $$ /$$__  $$| $$      | $$  | $$| $$  | $$\n| $$  | $$|  $$$$$$$| $$      |  $$$$$$/| $$  | $$\n|__/  |__/ \\_______/|__/       \\______/ |__/  |__/\n");
+         */
+        
+        /*****P1.3
+        System.out.println("  /////\n | o o |\n(|  ^  |)\n | (_) |\n  -----\n");
+         */
+        
+        /*****P1.4
+        System.out.println("  ------\n | Hello! |\n  ------\n  __      _\no\'\')}____//\n `_/      )\n (_(_/-(_/\n");
+         */
+        
+        /*****P1.5
+        System.out.println("+---+---+---+\n|   |   |   |\n+---+---+---+\n|   |   |   |\n+---+---+---+\n|   |   |   |\n+---+---+---+\n");
+        */
+       
+        /*****Question 1
+         * There will be 6 lines of output produced.
+         */
+        
+        /*****Question 2
+        public class Tricky {
+            public static void main(String[] args) {
+                System.out.println("Hello world");
+                System.out.println("Do you like this program?");
+                System.out.println();
+                System.out.println("I wrote it myself.");
+            }
+        }
+         */
+    }
+}
