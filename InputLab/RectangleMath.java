6bb259269f3346d71c452de135c6c2b8a445ecb1
Aaron Becker <aaron.becker@gmail.com>
2019-08-22 08:17:46 -0700

    finalized

--- InputLab/RectangleMath.java
+++ InputLab/RectangleMath.java
@@ -13,8 +13,10 @@ public class RectangleMath {
         double diagonalLength = Math.sqrt(Math.pow((double)side1,2)+Math.pow((double)side2,2)); //square each side first, then sqrt
         System.out.println("Area of triangle: "+(side1*side2)+"\nPerimeter of triangle: "+((2*side1)+(2*side2))+"\nDiagonal length: "+diagonalLength);
         
-        /*****QUESTION & ANSWER
-         * The first statement sequence
+        /*****QUESTION & ANSWER / VOCAB CHECK
+         * Q1) The first statement sequence is incorrect because it asks for 2 inputs, when the user is only prompted for 1.
+         * Q2) The next statement sequence's problem is that the value is recorded into a double but the user is only prompted for an int type.
+         * Vocab Check: Scanner, Strings, Objects, Classes, Method, Main Method, Package, Concatenate, Variable
          */
     }
     
