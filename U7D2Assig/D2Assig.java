255f6d5a8ba9ef9471dba261b4e753437d2ea806
Aaron Becker <aaron.becker@gmail.com>
2020-01-08 10:07:11 -0800

    all the stuff

---
+++ U7D2Assig/D2Assig.java
@@ -0,0 +1,75 @@
+public class D2Assig {
+    public static void main(String[] args) {
+        /****
+         * 1) A few applications include storing more than one piece of information about an object at a time.
+         * 2) The type of m[0] is int
+         * 3) You get the numbers of rows and columns from the constructor i.e. = new int[rows][columns]
+         * 4) An algorithm for inserting a value into a sorted array would be first to move everything in the array past the insertion point over by 1, and then replace the element at the insertion point with the new element.
+         * 5) The enhanced for loop counts how many times 0 appears in an array
+         * 6) for (int outer : TwoDArray) {
+         *      for (int inner : outer) {
+         *          System.out.println(inner);
+         *      }
+         *    }
+         * 7) It's not an appropriate shortcut because you can't modify elements in an enhanced for loop
+         * 8) int zeroCounter = 0;
+         * for (int s : array) {
+         *     if (s == 0) {
+         *         zeroCounter++;
+         *     }
+         * }
+         * 10a)
+         * for (int s : array) {
+         *     System.out.println(s+" ");
+         * }
+         * 10b)
+         * int max = array[0];
+         * for (int s : array) {
+         *     if (s > max) {
+         *         max = s;
+         *     }
+         * }
+         * 10c)
+         *  int nCount = 0;
+         *  for (int s : array) {
+         *      if (s< 0) {
+         *          nCount++;
+         *      }
+         *  }
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+    }
+}
